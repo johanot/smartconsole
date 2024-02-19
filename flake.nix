57da8bbfcd2e2d15d40f0f2d46933c0672654d4d
Johan Thomsen <joho@nnit.com>
2024-02-19 11:26:17 +0100

    nixos: 23.05 -> 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "smartconsole";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
   };
